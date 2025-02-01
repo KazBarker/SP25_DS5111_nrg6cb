9d9f05d88a95a4804ce42f549d7c456f756bde2e
KazBarker <142768289+KazBarker@users.noreply.github.com>
2025-02-01 17:39:58 -0500

    Create setup_git_global_creds.sh
    Added git creds file - debugging possible character problem encountered with writing via nano

---
+++ scripts/setup_git_global_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=kbarker747@gmail.com
+NAME=KazBarker
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
