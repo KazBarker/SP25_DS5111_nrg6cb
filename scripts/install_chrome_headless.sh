0aa857610c9620baa9f1c578cfd6a635c00293dc
KazBarker <kbarker747@gmail.com>
2025-02-05 01:25:20 +0000

    Minor updates

--- scripts/install_chrome_headless.sh
+++ scripts/install_chrome_headless.sh
@@ -1,9 +1,9 @@
 sudo apt update
 sudo apt install fonts-liberation -y
-sudo wget --no-clobber -P ~/chrome/ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ~/chrome/google-chrome-stable_current_amd64.deb -y
+sudo wget --no-clobber -P ../installations/chrome/ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ../installations/chrome/google-chrome-stable_current_amd64.deb -y
 sudo apt --fix-broken install
-sudo apt install ~/chrome/google-chrome-stable_current_amd64.deb -y
+sudo apt install ../installations/chrome/google-chrome-stable_current_amd64.deb -y
 
 # Check Version of chrome and run a quick sanity check
 google-chrome-stable --version
