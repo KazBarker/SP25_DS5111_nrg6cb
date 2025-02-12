49794841b73d89a42464c44e65060a7a0e285c8b
KazBarker <kbarker747@gmail.com>
2025-02-12 02:52:09 +0000

    progress on extracting ygainers

--- bin/normalize_csv.py
+++ bin/normalize_csv.py
@@ -2,14 +2,21 @@ import os
 
 import sys
 import pandas as pd
+import re
 
 def import_wjsgainers(wjs_data):
 	assert len(wjs_data.columns) == 6, f"Expected 6 columns, found {len(wjs_data.columns)}"
 	print("Found WJS Data\n")
+	print(f"\n{wjs_data.columns}\n")
 	print(wjs_data.head(5))
 
 def import_ygainers(y_data):
 	assert len(y_data.columns) == 13, f"Expected 13 columns, found {len(y_data.columns)}"
+	
+	y_data = y_data[['Symbol', 'Price', 'Change', 'Change %']].rename(columns={'Symbol':'symbol', 'Price':'price', 'Change':'price_change', 'Change %':'price_percent_change'})
+	y_data['price'] = y_data['price'].str.split(' ').str[0]
+	y_data['price_percent_change'] = y_data['price_percent_change'].replace(r'[^0-9.]', '', regex=True)
+	
 	print("Found YFinance Data\n")
 	print(y_data.head(5))
 
