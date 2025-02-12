1de5dae2285329bb8238bebb897ea76e929983d9
KazBarker <kbarker747@gmail.com>
2025-02-12 03:43:03 +0000

    Added additional assertions

--- bin/normalize_csv.py
+++ bin/normalize_csv.py
@@ -14,8 +14,8 @@ def import_ygainers(y_data):
 	assert len(y_data.columns) == 13, f"\nExpected 13 columns, found {len(y_data.columns)}\n"
 
 	y_data = y_data[['Symbol', 'Price', 'Change', 'Change %']].rename(columns={'Symbol':'symbol', 'Price':'price', 'Change':'price_change', 'Change %':'price_percent_change'})
-	y_data['price'] = y_data['price'].str.split(' ').str[0]
-	y_data['price_percent_change'] = y_data['price_percent_change'].replace(r'[^0-9.]', '', regex=True)
+	y_data['price'] = pd.to_numeric(y_data['price'].str.split(' ').str[0])
+	y_data['price_percent_change'] = pd.to_numeric(y_data['price_percent_change'].replace(r'[^0-9.]', '', regex=True))
 
 	return y_data
 
@@ -28,6 +28,11 @@ except: print("\nThere was a problem importing the data: make sure your file is 
 
 if data_name == "wjsgainers": normalized_data = import_wjsgainers(raw_data)
 if data_name == "ygainers": normalized_data = import_ygainers(raw_data)
+
+assert isinstance(normalized_data['symbol'][0], str), f'Expected "symbol" to contain string type, but instead found type: {type(normalized_data["symbol"][0])}'
+assert isinstance(normalized_data['price'][0], float), f'Expected "price" to contain float type, but instead found type: {type(normalized_data["price"][0])}'
+assert isinstance(normalized_data['price_change'][0], float), f'Expected "price_change" to contain float type, but instead found type: {type(normalized_data["price_change"][0])}'
+assert isinstance(normalized_data['price_percent_change'][0], float), f'Expected "price_percent_change" to contain float type, but instead found type: {type(normalized_data["price_percent_change"][0])}'
 
 write_path = re.sub('[.]csv$', '_norm.csv', data_path)
 normalized_data.to_csv(write_path, index=False)
