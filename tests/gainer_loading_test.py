0b59bc38e29aaf561beaee6a53c6680df94ae88f
KazBarker <kbarker747@gmail.com>
2025-03-13 15:44:03 +0000

    pylint improvements

--- tests/gainer_loading_test.py
+++ tests/gainer_loading_test.py
@@ -1,40 +1,62 @@
+'''
+Tests for the Gainer Factory
+
+These tests simulate the gainer factory process without an html download. They
+rely on the "test" methods, for which random data is generated - this random
+data is then processed identically to other gainers data (only missing the html
+step)
+'''
 import os
+from datetime import datetime
 import pytz
 import pandas as pd
 import pandas.api.types as ptypes
-import pathlib as pl
-from datetime import datetime
 from bin.gainers.factory import GainerFactory
 
 def test_gainer_download():
+    '''
+    Checks that the Gainer Factory saves a csv file when downloader.download() is
+    called.
+    '''
     factory = GainerFactory('test')
+
     downloader = factory.get_downloader()
-    normalizer = factory.get_processor()
-    
     downloader.download()
 
-    assert any([filename == 'testgainers.csv' for filename in os.listdir('../files')])
+    assert any(iter(filename == 'testgainers.csv' for filename in os.listdir('../files')))
 
 def test_gainer_normalize_and_save():
+    '''
+    Checks that the Gainer Factory creates a timestamped csv file when the 
+    normalizer methods are called.
+    '''
     factory = GainerFactory('test')
     normalizer = factory.get_processor()
 
     normalizer.normalize()
     normalizer.save_with_timestamp()
 
-    partial_name = f'test_gainers_{datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d-%H')}'
+    partial_name = f'test_gainers_{
+    datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d-%H')}'
 
-    assert any([filename.startswith(partial_name) for filename in os.listdir('../files')])
+    assert any(iter(filename.startswith(partial_name) for filename in os.listdir('../files')))
 
 def test_gainer_format():
-    partial_name = f'test_gainers_{datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d-%H')}'
+    '''
+    Checks that the Gainer Factory has created a timestamped csv file with the correct
+    column formats. This final test removes the testing csv file when executed.
+    '''
+    partial_name = f'test_gainers_{
+    datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d-%H')}'
 
     for file in os.listdir('../files'):
         if file.startswith(partial_name):
             test_data = pd.read_csv(f'../files/{file}')
             os.system(f'rm -f ../files/{file}')
 
-    checks = [ptypes.is_numeric_dtype(test_data[cc]) for cc in ['price', 'price_change','price_percent_change']]
+    checks = [ptypes.is_numeric_dtype(test_data[cc]) for cc in
+              ['price', 'price_change','price_percent_change']]
+
     checks.append(ptypes.is_string_dtype(test_data['symbol']))
 
     assert all(checks)
