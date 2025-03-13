fe47ed117239928da762d8ece0a5bf733719fda3
KazBarker <kbarker747@gmail.com>
2025-03-13 15:18:58 +0000

    added tests

--- tests/gainer_loading_test.py
+++ tests/gainer_loading_test.py
@@ -1,6 +1,7 @@
 import os
 import pytz
 import pandas as pd
+import pandas.api.types as ptypes
 import pathlib as pl
 from datetime import datetime
 from bin.gainers.factory import GainerFactory
@@ -14,7 +15,7 @@ def test_gainer_download():
 
     assert any([filename == 'testgainers.csv' for filename in os.listdir('../files')])
 
-def test_gainer_normalize():
+def test_gainer_normalize_and_save():
     factory = GainerFactory('test')
     normalizer = factory.get_processor()
 
@@ -25,22 +26,14 @@ def test_gainer_normalize():
 
     assert any([filename.startswith(partial_name) for filename in os.listdir('../files')])
 
-'''
-        # set output path with current timestamp
-        timestamp = datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d-%H:%M')
-        out_path = f'../files/{self.name}_gainers_{timestamp}.csv'
+def test_gainer_format():
+    partial_name = f'test_gainers_{datetime.now(pytz.timezone('America/New_York')).strftime('%Y-%m-%d-%H')}'
 
+    for file in os.listdir('../files'):
+        if file.startswith(partial_name):
+            test_data = pd.read_csv(f'../files/{file}')
 
-    assert pl.Path(
-    downloaded = pd.read_csv('../../files/
-        if not pl.Path(path).resolve().is_file():
-            raise AssertionError("File does not exist: %s" % str(path))
+    checks = [ptypes.is_numeric_dtype(test_data[cc]) for cc in ['price', 'price_change','price_percent_change']]
+    checks.append(ptypes.is_string_dtype(test_data['symbol']))
 
-
-
-
-def test_gainer_normalize():
-    normalizer.normalize()
-    normalizer.save_with_timestamp()
-
-   ''' 
+    assert all(checks)
