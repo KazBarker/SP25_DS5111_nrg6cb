467bc3c884de71f4fffe2d271843de5e12acc23a
KazBarker <kbarker747@gmail.com>
2025-04-29 15:21:21 -0400

    pylint cleaning

--- tests/gainer_snowflake_test.py
+++ tests/gainer_snowflake_test.py
@@ -1,15 +1,15 @@
 '''
 Tests the export pipeline that pushes normalized gainer files to Snowflake.
 '''
-import os
 import pytest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from bin.snowflake.flake_gainers import SnowflakeGainers
 
 @pytest.mark.parametrize(
-        'test_csv, expected_csv',
-        [('tests/sample_data/test_gainers_1900-01-01-09:31.csv', 'tests/sample_data/expected_frame.csv')])
+        'test_csv, expected_csv', [(
+            'tests/sample_data/test_gainers_1900-01-01-09:31.csv',
+            'tests/sample_data/expected_frame.csv')])
 def test_get_frame(test_csv, expected_csv):
     '''
     Tests that a CSV in the expected format is parsed correctly and returned as
